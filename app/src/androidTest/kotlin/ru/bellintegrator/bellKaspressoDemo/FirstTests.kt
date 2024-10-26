dbf047363309b9478a260ae89a576f8a74894678
Ruslan Borodin <RSBorodin2@sberbank.ru>
2024-10-26 05:44:15 +0300

    project init

--- app/src/androidTest/kotlin/ru/bellintegrator/bellKaspressoDemo/FirstTests.kt
+++ app/src/androidTest/kotlin/ru/bellintegrator/bellKaspressoDemo/FirstTests.kt
@@ -1,11 +1,13 @@
 import androidx.test.ext.junit.rules.ActivityScenarioRule
+import com.kaspersky.components.alluresupport.withForcedAllureSupport
+import com.kaspersky.kaspresso.kaspresso.Kaspresso
 import com.kaspersky.kaspresso.testcases.api.testcase.TestCase
 import org.junit.Rule
 import org.junit.Test
 import pages.MainPage
 import ru.bellintegrator.bellKaspressoDemo.MainActivity
 
-class FirstTests : TestCase() {
+class FirstTests : TestCase(kaspressoBuilder = Kaspresso.Builder.withForcedAllureSupport()) {
     @get:Rule
     val activityRule = ActivityScenarioRule(MainActivity::class.java)
 
